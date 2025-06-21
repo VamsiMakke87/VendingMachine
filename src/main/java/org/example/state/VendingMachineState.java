e106fdf2a63c3422ed65b20095f61ba700068ca1
vamsimakke87 <vamsimakke@gmail.com>
2025-06-21 16:03:25 -0400

    Added Vending Machine states

--- src/main/java/org/example/state/VendingMachineState.java
+++ src/main/java/org/example/state/VendingMachineState.java
@@ -1,4 +1,15 @@
 package org.example.state;
 
 public interface VendingMachineState {
+
+    public void selectProduct(Pr);
+
+    public void insertCoin();
+
+    public void insertNote();
+
+    public void dispenseProduct();
+
+    public void returnChange();
+
 }
