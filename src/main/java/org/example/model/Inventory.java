2284054b1a9b68fee5dd1a75e911e7ad6264432b
vamsimakke87 <vamsimakke@gmail.com>
2025-06-21 16:02:51 -0400

    Added Product and Inventory class

--- src/main/java/org/example/model/Inventory.java
+++ src/main/java/org/example/model/Inventory.java
@@ -1,4 +1,40 @@
 package org.example.model;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Inventory {
+
+    private Map<Product, Integer> productInventory;
+
+    private Inventory instance;
+
+    private Inventory() {
+        productInventory = new LinkedHashMap<>();
+    }
+
+    public synchronized Inventory getInstance() {
+        if (instance == null) {
+            instance = new Inventory();
+        }
+        return instance;
+    }
+
+    public void addProduct(Product product, int quantity) {
+        productInventory.put(product, productInventory.getOrDefault(product, 0) + quantity);
+    }
+
+    public int getQuantity(Product product) {
+        return productInventory.getOrDefault(product, 0);
+    }
+
+    public boolean hasStock(Product product,int quantity) {
+        return getQuantity(product) > quantity;
+    }
+
+    public void reduceQuantity(Product product,int quantity){
+        productInventory.put(product,productInventory.get(product)-quantity);
+    }
+
+
 }
